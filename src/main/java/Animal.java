a47fb5ea8b918585a9dc21f356c5aece43ecd501
agnes musyoka <kavatamusyoka24@gmail.com>
2018-05-28 16:33:16 +0300

    add saving function

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -1,29 +1,41 @@
+import org.sql2o.*;
+
 public class Animal {
     public String name;
     public String endangered;
     public int id;
-  
+
     public Animal(String name, String endangered) {
-      this.name = name;
-      this.endangered = endangered;
+        this.name = name;
+        this.endangered = endangered;
     }
-  
+
     public String getName() {
-      return name;
+        return name;
     }
-  
+
     public String getEndangered() {
-      return endangered;
+        return endangered;
     }
 
     @Override
 
-  public boolean equals(Object otherAnimal) {
-    if (!(otherAnimal instanceof Animal)) {
-      return false;
-    } else {
-      Animal newAnimal = (Animal) otherAnimal;
-      return this.getName().equals(newAnimal.getName());
+    public boolean equals(Object otherAnimal) {
+        if (!(otherAnimal instanceof Animal)) {
+            return false;
+        } else {
+            Animal newAnimal = (Animal) otherAnimal;
+            return this.getName().equals(newAnimal.getName());
+        }
     }
-  }
+
+    public void save() {
+        try(Connection con = DB.sql2o.open()) {
+          String sql = "INSERT INTO animals (name, endangered) VALUES (:name, :endangered)";
+          con.createQuery(sql)
+            .addParameter("name", this.name)
+            .addParameter("endangered", this.endangered)
+            .executeUpdate();
+        }
+      }
 }
--- src/main/test/java/AnimalTest.java
+++ src/main/test/java/AnimalTest.java
@@ -5,6 +5,9 @@ import org.sql2o.*;
 
 public class AnimalTest {
 
+    @Rule
+  public DatabaseRule database = new DatabaseRule();
+
   @Test
   public void animal_instantiatesCorrectly_true() {
     Animal testAnimal = new Animal("Lion");
